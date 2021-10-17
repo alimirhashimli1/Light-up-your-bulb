76b581004ce45b22b849e0e8580f842cb91a1470
alimirhashimli <alimirhashimli@gmail.com>
2021-10-17 23:00:05 +0200

    added the project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Light-up-your-bulb
 Small bulb switches on and off with a toggle button
+
+![before](before.png)
+
+![before](after.png)
---
+++ app.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    const bulb = document.querySelector(".far")
+    bulb.classList.toggle("mystyle")
+ console.log(bulb)
+}
